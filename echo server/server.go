3e83009006063a6e997893f4786492286ffd7011
Ronaldo <rraragaomonteiro@gmail.com>
2024-05-27 06:56:29 -0300

    Update server.go

--- echo server/server.go
+++ echo server/server.go
@@ -4,12 +4,22 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"log"
 	"net"
 	"os"
 	"strings"
 )
 
+var logger *log.Logger
+
 func main() {
+	// Open log file
+	logFile, err := os.OpenFile("echo_server.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Println("Failed to open log file:", err)
+		os.Exit(1)
+	}
+	logger = log.New(logFile, "", log.LstdFlags)
 
 	port := fmt.Sprintf(":%s", os.Args[1])
 	prefix := os.Args[2]
@@ -17,12 +27,12 @@ func main() {
 
 	listener, err := net.Listen("tcp", port)
 	if err != nil {
-		fmt.Println("server initialization failed, error:", err)
+		fmt.Println("Server initialization failed, error:", err)
 		os.Exit(1)
 	}
 	defer listener.Close()
 
-	fmt.Println("server initialized in port:", port)
+	fmt.Println("Server initialized on port:", port)
 
 	connections := make(chan net.Conn)
 
@@ -30,7 +40,7 @@ func main() {
 		for {
 			conn, err := listener.Accept()
 			if err != nil {
-				fmt.Println("failed to accpet connection, error:", err)
+				fmt.Println("Failed to accept connection, error:", err)
 				close(connections)
 				return
 			}
@@ -41,7 +51,6 @@ func main() {
 				continue
 			}
 			connections <- conn
-
 		}
 	}()
 
@@ -51,21 +60,19 @@ func main() {
 }
 
 func HandleConnections(conn net.Conn, prefix string) {
-
 	defer conn.Close()
 	reader := bufio.NewReader(conn)
 	for {
 		bytes, err := reader.ReadBytes(byte('\n'))
 		if err != nil {
 			if err != io.EOF {
-				fmt.Println("failed to read data, error:", err)
+				fmt.Println("Failed to read data, error:", err)
 			}
 			return
 		}
-		fmt.Printf("request: %s", bytes)
-		line := string(bytes)
-		fmt.Printf("response: %s", line)
-		conn.Write([]byte(line))
+		request := fmt.Sprintf("%s", bytes)
+		response := request                                             // Assuming the response is simply echoing the request
+		logger.Printf("Request: %s\nResponse: %s\n", request, response) // Log the request and response
+		conn.Write([]byte(response))
 	}
-
 }
